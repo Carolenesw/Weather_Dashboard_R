1c7815ebb9493ca3ce5c9c317c2bcae53423a2e0
Carolene Wilson <carolenesw@gmail.com>
2020-05-27 16:15:17 -0400

    use varibales to capture data from api call

--- Asset/script.js
+++ Asset/script.js
@@ -12,8 +12,7 @@ $(document).ready(function () {
         // AJAX call to the run OpenWeatherMap API for 5 days forecast
         // var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q={cityName}&appid={apiKey}`;
 
-        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=e03d5c93b258a3dd162f1dc56d7449bd";
-
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + apiKey;
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -23,27 +22,38 @@ $(document).ready(function () {
             console.log(data.list[0].weather[0].icon)
 
 
-            
-            console.log(iconCode)
+            // $(".icon").html("<img src='http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
 
-            var iconUrl = "http://openweathermap.org/img/wn/" + iconCode;
-
-            // http://openweathermap.org/img/wn/10d@2x.png
-            console.log(iconUrl)
-            // $(".icon").html("<img src='" + iconUrl  + "'>");
-           
-
+            // use variables to capture required data from API call
             var iconCode = "<img src='http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png' alt='Icon depicting current weather.'>";
             console.log(iconCode)
+
+            var currentDate = data.list[0].dt_txt;
+            console.log(currentDate)
+
+            var temp = data.list[0].main.temp;
+            console.log(temp)
+            var humD = data.list[0].main.humidity;
+            console.log(humD)
+            var windS = data.list[0].wind.speed
+            console.log(windS)
+
+
+
             //  set city search result to local storage
             localStorage.setItem("city", cityName)
             localStorage.setItem("icon", iconCode)
 
         })
     })
-    
-    localStorage.getItem("<img src='http://openweathermap.org/img/w/04d.png' alt='Icon depicting current weather.'>")
-    $(".icon").append("<img src='http://openweathermap.org/img/w/04d.png' alt='Icon depicting current weather.'>");
+
+    // localStorage.getItem("<img src='http://openweathermap.org/img/w/04d.png' alt='Icon depicting current weather.'>")
+    // $(".icon").append("<img src='http://openweathermap.org/img/w/04d.png' alt='Icon depicting current weather.'>");
+
+
+
+    // localStorage.getItem("<img src='http://openweathermap.org/img/w/04d.png' alt='Icon depicting current weather.'>")
+    // $(".icon").append("<img src='http://openweathermap.org/img/w/04d.png' alt='Icon depicting current weather.'>");
 })
 
 
