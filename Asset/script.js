e0e54e3c2f73e2080e7bfdd4ca77e1fe67cac58b
Carolene Wilson <carolenesw@gmail.com>
2020-05-27 14:13:49 -0400

    create click function to call make ajax call

--- Asset/script.js
+++ Asset/script.js
@@ -1,22 +1,26 @@
 $(document).ready(function () {
 
     var apiKey = "e1014510ebbf942b1f1d07d44fa4f59b";
-    var cityName =  $(".search");
-    // var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q={cityName}&appid={apiKey}`;
+    var cityName = "";
 
-    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName},Jamaica&appid=${apiKey}`;
+    // click function to capture city search and call weather app API
+    $(".save").on("click", function (event) {
+        event.preventDefault();
+        cityName = $(".search").val().trim();
+        console.log(cityName)
 
-   
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (data) {
-        console.log(data)
+        // AJAX call to the run OpenWeatherMap API for 5 days forecast
+        // var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q={cityName}&appid={apiKey}`;
 
-        $(".save").on("click", function (event) {
-            event.preventDefault();
-            cityName = cityName.val();
-            console.log(cityName)
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=e03d5c93b258a3dd162f1dc56d7449bd";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (data) {
+            console.log(data)
+
+
         })
     })
 })
