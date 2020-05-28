3133a1efffccd237ec8f1e4d0737dc5e572daf6f
Carolene Wilson <carolenesw@gmail.com>
2020-05-28 13:09:01 -0400

    push city search list to array

--- Asset/script.js
+++ Asset/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
 
     var apiKey = "e1014510ebbf942b1f1d07d44fa4f59b";
-    var cityName = "";
+    var cityList = "";
 
+    // create empty array to store city searches
+    var cityList = [];
     // use to moment.js to add and change dates
     var currentDate = moment().format('L');
     var date1 = moment().add(1, 'days').format('L');
@@ -39,7 +41,7 @@ $(document).ready(function () {
 
             // use variables to capture required data from API call
             // var iconCode = "<img src='http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png' alt='Icon depicting current weather.'>";
-            var iconCode =  $(".icon").html("<img src='http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png' alt=''>");
+            var iconCode = $(".icon").html("<img src='http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png' alt=''>");
             // var iconCode = "<img src=http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png"+ ">";
             console.log(iconCode)
             var temp = data.list[0].main.temp;
@@ -48,22 +50,30 @@ $(document).ready(function () {
             console.log(humD)
             var windS = data.list[0].wind.speed
             console.log(windS)
+            // use city name from api call to render list
+            var cityS = data.city.name
 
             //  set current city search result to local storage
             localStorage.setItem("city", cityName + " " + "(" + currentDate + ")");
+            localStorage.setItem("cityS", cityS);
             localStorage.setItem("icon", JSON.stringify(iconCode));
             localStorage.setItem("date", currentDate);
             localStorage.setItem("temp", "Temperature: " + temp + "F");
             localStorage.setItem("humidity", "Humidity: " + humD + "%");
             localStorage.setItem("wind", "Wind Speed: " + windS + "MPH");
 
+            renderLastSearch();
         })
+
     })
 
+
+    // render currentr forecast function to display search result on page
+    renderCurrentDayForecast();
     function renderCurrentDayForecast() {
         var city = localStorage.getItem("city");
         var icon = JSON.parse(localStorage.getItem("icon"));
-        // var date = localStorage.getItem("date");
+        var date = localStorage.getItem("date");
         // console.log(date)
         var temp = localStorage.getItem("temp");
         var hum = localStorage.getItem("humidity");
@@ -71,18 +81,29 @@ $(document).ready(function () {
         // console.log(city)
         if (city) {
             $(".city").text(city);
-            // $(".date").text(date);
+            $(".date").text(date);
             $(".icon").text(icon);
             $(".temp").text(temp);
             $(".humidity").text(hum);
             $(".wind").text(wind);
         }
-        else {
-            return;
-        }
+        // else {
+        //     return;
+        // }
 
     }
-    renderCurrentDayForecast()
+
+    // create function to render list of previous searches
+    function renderLastSearch() {
+        // get city list from local storage and push to array
+        var cityS = localStorage.getItem("cityS")
+        console.log(cityS)
+        cityList.push(cityS)
+        console.log(cityList)
+        
+    }
+
+
     // localStorage.getItem("<img src='http://openweathermap.org/img/w/04d.png' alt='Icon depicting current weather.'>")
     // $(".icon").append("<img src='http://openweathermap.org/img/w/04d.png' alt='Icon depicting current weather.'>");
 
