510178b46f5f10646f4fe6592c7779ba7094f1d1
Carolene Wilson <carolenesw@gmail.com>
2020-05-27 21:27:08 -0400

    set items to local storage and use moment.js to set dates

--- Asset/script.js
+++ Asset/script.js
@@ -3,6 +3,16 @@ $(document).ready(function () {
     var apiKey = "e1014510ebbf942b1f1d07d44fa4f59b";
     var cityName = "";
 
+     // use to moment.js to add and change dates
+     var currentDate = moment().format('L');
+     var date1 = moment().add(1, 'days').format('L'); 
+     var date2 = moment().add(2, 'days').format('L')
+     var date3 = moment().add(3, 'days').format('L')
+     var date4 = moment().add(4, 'days').format('L')
+     var date5 = moment().add(5, 'days').format('L')
+     console.log(date5)
+     console.log(currentDate)
+
     // click function to capture city search and call weather app API
     $(".save").on("click", function (event) {
         event.preventDefault();
@@ -27,10 +37,6 @@ $(document).ready(function () {
             // use variables to capture required data from API call
             var iconCode = "<img src='http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png' alt='Icon depicting current weather.'>";
             console.log(iconCode)
-
-            var currentDate = data.list[0].dt_txt;
-            console.log(currentDate)
-
             var temp = data.list[0].main.temp;
             console.log(temp)
             var humD = data.list[0].main.humidity;
@@ -38,11 +44,13 @@ $(document).ready(function () {
             var windS = data.list[0].wind.speed
             console.log(windS)
 
-
-
-            //  set city search result to local storage
-            localStorage.setItem("city", cityName)
-            localStorage.setItem("icon", iconCode)
+            //  set current city search result to local storage
+            localStorage.setItem("city", cityName);
+            localStorage.setItem("icon", iconCode);
+            localStorage.setItem("date", currentDate);
+            localStorage.setItem("temp", "Temperature: " + temp + "F");
+            localStorage.setItem("humidity", "Humidity: " + humD + "%");
+            localStorage.setItem("wind", "Wind Speed: " + windS + "MPH");
 
         })
     })
